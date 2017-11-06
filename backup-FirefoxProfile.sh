93b0f1cdf6f95e38d368d102132823d771c3dc11
Celmor <celmor@mail.com>
2017-11-06 19:11:49 +0100

    Adds argument handling

--- backup-FirefoxProfile.sh
+++ backup-FirefoxProfile.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-profile=("${HOME}"/.mozilla/firefox/*.default)
-gzip < "$profile"/places.sqlite > "${HOME}"/backup/ff/"${profile##*/}".places.sqlite."$date".gz
+[[ "$1" == *-h* ]] && prinf 'Usage: %s [PROFILE] [BACKUPPATH] [SUFFIX]\n' "$0"
+Profile=("${HOME}"/.mozilla/firefox/${1:-*.default})
+BackupPath="${2:-./backup/ff}"
+Suffix="${3:-"$(date --iso-8601=second)"}"
+gzip < $Profile/places.sqlite > "$BackupPath"/"$(basename $Profile)".places.sqlite."$Suffix".gz
