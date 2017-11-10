e70bd97cf3826f4d9159f713ae9ad22b93c07b00
Celmor <celmor@mail.com>
2017-11-10 23:51:33 +0100

    Files to be added

--- backup-FirefoxProfile.sh
+++ backup-FirefoxProfile.sh
@@ -3,4 +3,11 @@
 Profile=("${HOME}"/.mozilla/firefox/${1:-*.default})
 BackupPath="${2:-./backup/ff}"
 Suffix="${3:-"$(date --iso-8601=second)"}"
+http://kb.mozillazine.org/Profile_folder_-_Firefox
+Files=(
+	"cert_override.txt"
+	"places.sqlite"
+	"permissions.sqlite"
+	"$(ls -t bookmarkbackups/bookmarks-* | head -1)"
+)
 gzip < $Profile/places.sqlite > "$BackupPath"/"$(basename $Profile)".places.sqlite."$Suffix".gz
