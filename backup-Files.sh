97d303dd3b7812edb7bdb1f0047558e95f5a9d92
Celmor <celmor@mail.com>
2017-12-08 20:25:36 +0100

    fix arguments

--- backup-Files.sh
+++ backup-Files.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 if [ "$1" == "-h" ]; then
-	printf "Usage: %s FILE...\\n" "$0"
+	printf "Usage: %s BACKUPPATH REMOTESHELL FILE...\\n" "$0"
 	exit 0
 fi
-BackupPath="${1:-"root@10.0.1.21:/Data/Backup/daily"}"
+[ "$1" = "" ] && BackupPath="root@10.0.1.21:/Data/Backup/daily" || BackupPath="$1"
+[ "$2" = "" ] && RemoteShell="ssh" || BackupPath="$2"
+
+shift 2
 printf "BackupPath = %s\\n" "$BackupPath"
 printf "RemoteShell = %s\\n" "$RemoteShell"
 
 for file in "$@"; do
-	rsync -e "$RemoteShell" -i -ahxHAX --delete "$file" ${BackupPath}/
+	rsync -e "$RemoteShell" -i -ahxHAX --delete "$file" "$BackupPath"/"$file"
 done
