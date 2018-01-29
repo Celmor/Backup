99ff2dfabf968f4024433c41149d798cc136ceef
Celmor <celmor@mail.com>
2018-01-29 23:59:03 +0100

    fixes parameters; adds checks

--- backup-Type.sh
+++ backup-Type.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-cd "$1"
+SearchDir="$1"
+archive="$2"
+type="$3"
+[ -d "$SearchDir" ] || [ ! -f "$archive" ] || [ "" != "$type" ] || { \
+	printf "Usage: %s SearchDir archive type\\n" "$0"
+	exit -1
+}
+
+cd "$SearchDir"
 printf %s\\n "searching files..."
 files=(); while read -rd '' file && read -rn1 && read -r type; do if [[ $type = ?(;*) ]]; then files+=("$file"); fi; done < <(find . -type f -exec file -0i {} +); declare -p files
-tar -cJf "$3" -T <(printf %s\\n "${files[@]}")
+tar -cJf "$archive" -T <(printf %s\\n "${files[@]}")
