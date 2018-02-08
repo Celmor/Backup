d867e768b098709d166bf8ad159b24dea0ecff99
Celmor <celmor@mail.com>
2018-02-08 23:58:28 +0100

    adds size parameter for du of files

--- backup-Small.sh
+++ backup-Small.sh
@@ -2,6 +2,7 @@
 SearchDir="$1"
 archive="$2"
 wildcard="$3"
+size="${4:-5120}"
 #size="$4"
 [ -d "$SearchDir" ] || [ ! -f "$archive" ] || [ "" != "$wildcard" ] || { \
 	printf "Usage: %s SearchDir archive [wildcard]\\n" "$0"
@@ -12,7 +13,7 @@ cd "$SearchDir"
 printf %s\\n "searching Items..."
 items=()
 while read -rd '' item; do
-	if [ "$(du -cx ~/sh/Backup/.git | awk 'END { print $1 }')" -lt 5120 ]; then # less than 5MiB
+	if [ "$(du -cx ~/sh/Backup/.git | awk 'END { print $1 }')" -lt "$size" ]; then # less than 5MiB or $size
 		items+=("$item")
 	fi
 done < <(find . -iname "$wildcard" -print0)
