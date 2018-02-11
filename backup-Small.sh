9aeff0d9a3a75143b16129fec0e01f9322a74b5e
Celmor <celmor@mail.com>
2018-02-11 17:43:14 +0100

    make paths of items added to tar archive relative to SearchDir

--- backup-Small.sh
+++ backup-Small.sh
@@ -8,17 +8,18 @@ archive="$2"
 wildcard="$3"
 size="${4:-1024}"
 
-[ -d "$SearchDir" ] && [ ! -f "$archive" ] && [ "" != "$wildcard" ] || { \
+[ -d "$SearchDir" ] && [ ! -f "$archive" ]  && [ "" != "$wildcard" ] || { \
 	printf "Usage: %s SearchDir archive [wildcard]\\n" "$0" >&2
 	exit -1
 }
-
+prefix="$PWD"
+cd "$SearchDir" || { printf "Error: Can't change dir to %s.\\n" "$SearchDir" >&2; exit 1; }
 printf %s\\n "Searching Items..." >&2
 items=()
 while read -rd '' item; do
 	if [ "$(du -cx "$item" | awk 'END { print $1 }')" -lt "$size" ]; then # less than 1MiB or $size
 		items+=("$item")
 	fi
-done < <(find "$SearchDir"/* -iname "$wildcard" ! -name "$archive" -print0 2>/dev/null)
+done < <(find ./* -iname "$wildcard" ! -name "$archive" -print0 2>/dev/null)
 printf %s\\n "Archiving Items..." >&2
-tar -cJf "$archive" -C "$SearchDir" -T <(printf %s\\n "${items[@]}")
+tar -cJf "$prefix"/"$archive" -T <(printf %s\\n "${items[@]}")
