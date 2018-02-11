cb9b27e338ca8003265f9362b423e900526826d2
Celmor <celmor@mail.com>
2018-02-11 17:57:50 +0100

    removes builtin compression for optional compression via piping

--- backup-Small.sh
+++ backup-Small.sh
@@ -2,24 +2,27 @@
 # Searches files matching "wildcard" (should at least be "*") in "SearchDir" of size below 1MiB
 #  or whatever "size" is set to (value in KiB units) and tars and compresses them
 # "archive" should not already exist, this script can't add files to a pre-existing archive
+# if archive name is "-" it will be written out to stdout, e.g. for compression
+# EXAMPLE:
+# ./Backup/backup-Small.sh ./Backup - "*" | xz > testb.tar.xz
+# tar -xJf Backup.tar.xz
+# xz -cdk Backup.tar.xz | tar -tf -
 
 SearchDir="$1"
 archive="$2"
 wildcard="$3"
 size="${4:-1024}"
 
-[ -d "$SearchDir" ] && [ ! -f "$archive" ]  && [ "" != "$wildcard" ] || { \
+[ -d "$SearchDir" ] && { [ ! -f "$archive" ] || [ "$archive" == "-" ]; } && [ "" != "$wildcard" ] || { \
 	printf "Usage: %s SearchDir archive [wildcard]\\n" "$0" >&2
 	exit -1
 }
-prefix="$PWD"
-cd "$SearchDir" || { printf "Error: Can't change dir to %s.\\n" "$SearchDir" >&2; exit 1; }
 printf %s\\n "Searching Items..." >&2
 items=()
 while read -rd '' item; do
-	if [ "$(du -cx "$item" | awk 'END { print $1 }')" -lt "$size" ]; then # less than 1MiB or $size
+	if [ "$(du -cx "$SearchDir"/"$item" | awk 'END { print $1 }')" -lt "$size" ]; then # less than 1MiB or $size
 		items+=("$item")
 	fi
-done < <(find ./* -iname "$wildcard" ! -name "$archive" -print0 2>/dev/null)
+done < <(cd "$SearchDir" && find ./* -iname "$wildcard" ! -name "$archive" -print0 2>/dev/null)
 printf %s\\n "Archiving Items..." >&2
-tar -cJf "$prefix"/"$archive" -T <(printf %s\\n "${items[@]}")
+tar -cf "$archive" -C "$SearchDir" -T <(printf %s\\n "${items[@]}")
