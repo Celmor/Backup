09a836cb2993c37ce64699592996d37961a66f51
Celmor <celmor@mail.com>
2018-02-28 23:20:43 +0100

    fix parameters, variables and usage

--- backup-Root.sh
+++ backup-Root.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
+#handle if variables RemoteShell and ExcludeFiles are unset
+
 if [ "$EUID" -ne 0 ]; then
 	printf "Error: This script must be run as root\\n"
 	exit -1
 fi
 Print-Usage(){
-	printf "Usage: %s [BackupPath] [RemoteShell] [ExcludeFiles] [Suffix]\\n" "$0"
+	printf "Usage: %s [-p <BackupPath>] [-e <RemoteShell>] [-f <ExcludeFiles>]\\n" "$0"
 	exit 0
 }
+if [ -f "$(which "$0").txt" ]; then
+	ExcludeFiles="$(which "$0").txt"
+fi
+BackupPath="/Data/Backup/Linux/NVMeRoot" #defines default BackupPath
 
-BackupPath="root@10.0.1.21:/Data/Backup/Linux/NVMeRoot"
-RemoteShell="ssh"
-ExcludeFiles="$(which "$0").txt"
-while getopts ":hvp:e:f:" arg; do
+while getopts ":p:e:f:" arg; do
   case $arg in
-    h)
-      Print-Usage
-      ;;
     p)
       BackupPath=${OPTARG}
       ;;
     e)
-      RemoteShell=${OPTARG}
+      RemoteShell="${OPTARG}"
       ;;
     f)
       ExcludeFiles=${OPTARG}
@@ -32,9 +32,12 @@ while getopts ":hvp:e:f:" arg; do
 done
 shift $((OPTIND-1))
 
-printf "BackupPath = %s\\n" "$BackupPath"
-printf "RemoteShell = %s\\n" "$RemoteShell"
-printf "ExcludeFiles = %s\\n" "$ExcludeFiles"
+if [ "$(df --output=source / | tail -1)" == "$(df --output=source "$BackupPath" | tail -1)" ]; then
+	printf "Warning: BackupPath (\"%s\") is on the same Filesystem as the Root Filesystem (\"/\")\\n" "$BackupPath"
+fi
+printf "BackupPath = \"%s\"\\n" "$BackupPath" >&2
+printf "RemoteShell = \"%s\"\\n" "$RemoteShell" >&2
+printf "ExcludeFiles = \"%s\"\\n" "$ExcludeFiles" >&2
 read -r -n 1 -p "Do you want to continue? [Y/n] " ans || { echo; exit; } && echo
 case "$ans" in
     [yY][eE][sS]|[yY]|"")
@@ -45,4 +48,4 @@ case "$ans" in
         ;;
 esac
 
-rsync -e "$RemoteShell" -i -ahxHAX --delete --delete-excluded --exclude-from=${ExcludeFiles} / ${BackupPath}/
+rsync ${RemoteShell:+-e "$RemoteShell"} -i -ahxHAX --delete --delete-excluded ${ExcludeFiles:+"--exclude-from=$ExcludeFiles"} / "$BackupPath"/
