2c0491ac973551568b37708ed718396d67c1132f
Celmor <celmor@mail.com>
2018-03-23 02:34:40 +0100

    refactor

--- backup-Root.sh
+++ backup-Root.sh
@@ -2,39 +2,39 @@
 #handle if variables RemoteShell and ExcludeFiles are unset
 
 if test "$EUID" -ne 0; then
-	printf "Error: This script must be run as root\\n"
-	exit -1
+    printf "Error: This script must be run as root\\n"
+    exit -1
 fi
 ################### FUNCTION ###################
 Print-Usage(){
-	printf "Usage: %s [-p <BackupPath>] [-e <RemoteShell>] [-f <ExcludeFiles>]\\n" "$0"
-	exit 0
+    printf "Usage: %s [-p <BackupPath>] [-e <RemoteShell>] [-f <ExcludeFiles>]\\n" "$0"
+    exit 0
 }
 function trapped {
-	printf \\n
-	lvm_exit
+    printf \\n
+    lvm_exit
 }
 function lvm_exit {
-	if test "$lvm"; then
-	  if mountpoint -q "$source"; then
-		umount "$source" || exit 1
-	  fi
-	  if test -b "$lvm_VG-${lvm_snap//-/--}"; then
-		lvremove -f "$(basename "$lvm_VG")/$lvm_snap" || exit 1
-	  fi
-	  if test -d "$source"; then
-		rm -d "$source"
-	  fi
-	  exit $1
-	fi
+    if test "$lvm"; then
+      if mountpoint -q "$source"; then
+        umount "$source" || exit 1
+      fi
+      if test -b "$lvm_VG-${lvm_snap//-/--}"; then
+        lvremove -f "$(basename "$lvm_VG")/$lvm_snap" || exit 1
+      fi
+      if test -d "$source"; then
+        rm -d "$source"
+      fi
+      exit $1
+    fi
 }
 
 ##################### VARS #####################
 if [ -f "$(which "$0").txt" ]; then
-	ExcludeFiles="$(which "$0").txt"
+    ExcludeFiles="$(which "$0").txt"
 fi
-BackupPath="/Data/Backup/Linux/NVMeRoot"	#defines default BackupPath
-source=/									#defines backup source
+BackupPath="/Data/Backup/Linux/NVMeRoot"    #defines default BackupPath
+source=/                                    #defines backup source
 
 while getopts ":p:e:f:ls:" arg; do
   case $arg in
@@ -50,9 +50,9 @@ while getopts ":p:e:f:ls:" arg; do
     s)
       source=${OPTARG}
       ;;
-	l)
-	  lvm="lvm"
-	  ;;
+    l)
+      lvm="lvm"
+      ;;
     \?)
       Print-Usage
       ;;
@@ -60,20 +60,20 @@ while getopts ":p:e:f:ls:" arg; do
 done
 shift $((OPTIND-1))
 if test "$lvm"; then
-	lvm="$(df --output=source "$source" | tail -1)"	#e.g. /dev/mapper/ssd-root
-	lvm_snap="$(basename "$lvm")"					#-> ssd-root
-	lvm_VG="${lvm%%-*}"								#-> /dev/mapper/ssd
-	lvm_snap="${lvm#*-}-snap-$(date +%F-%0H-%0M)"	#-> root-snap-d2018-03-16-13-31
-	source="$(mktemp -d /media/temp.XXX)" && \
-	test "$lvm_snap" && test "$lvm_VG" || lvm_exit 1
-	trap lvm_exit INT
+    lvm="$(df --output=source "$source" | tail -1)"    #e.g. /dev/mapper/ssd-root
+    lvm_snap="$(basename "$lvm")"                    #-> ssd-root
+    lvm_VG="${lvm%%-*}"                                #-> /dev/mapper/ssd
+    lvm_snap="${lvm#*-}-snap-$(date +%F-%0H-%0M)"    #-> root-snap-d2018-03-16-13-31
+    source="$(mktemp -d /media/temp.XXX)" && \
+    test "$lvm_snap" && test "$lvm_VG" || lvm_exit 1
+    trap lvm_exit INT
 fi
 
 ###################### UI ######################
 if [ "$(df --output=source / | tail -1)" == "$(df --output=source "$BackupPath" | tail -1)" ]; then
-	printf "Warning: BackupPath (\"%s\") is on the same Filesystem as the Root Filesystem (\"/\")\\n" "$BackupPath"
+    printf "Warning: BackupPath (\"%s\") is on the same Filesystem as the Root Filesystem (\"/\")\\n" "$BackupPath"
 fi
-printf	"${lvm:+lvm mapping: \t$lvm\\n}\
+printf    "${lvm:+lvm mapping: \t$lvm\\n}\
 ${lvm_snap:+lv snap: \t$(basename "$lvm_VG")/$lvm_snap\\n}\
 source: \t%s
 BackupPath: \t%s
@@ -91,9 +91,9 @@ esac
 
 ##################### EXEC #####################
 if test "$lvm"; then
-	lvcreate -L 5G --snapshot -n "$lvm_snap" "$lvm" && \
-	mount "$lvm_VG-${lvm_snap//-/--}" "$source" || lvm_exit 1
+    lvcreate -L 5G --snapshot -n "$lvm_snap" "$lvm" && \
+    mount "$lvm_VG-${lvm_snap//-/--}" "$source" || lvm_exit 1
 fi
-rsync ${RemoteShell:+-e "$RemoteShell"} -i -ahxHAX --delete --delete-excluded ${ExcludeFiles:+"--exclude-from=$ExcludeFiles"} "$source"/ "$BackupPath"/
+rsync ${RemoteShell:+-e "$RemoteShell"} -i -ahxHAX --delete ${ExcludeFiles:+"--exclude-from=$ExcludeFiles"} "$source"/ "$BackupPath"/
 lvm_exit
 
