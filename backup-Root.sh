5c65609d09f6074e1ef5671d3b1d41204790516b
Celmor <celmor@mail.com>
2018-04-10 15:55:17 +0200

    refactor & fixup

--- backup-Root.sh
+++ backup-Root.sh
@@ -2,19 +2,16 @@
 #handle if variables RemoteShell and ExcludeFiles are unset
 
 if test "$EUID" -ne 0; then
-    printf "Error: This script must be run as root\\n"
+    printf "Error: This script must be run as root\\n" >&2
     exit -1
 fi
 ################### FUNCTION ###################
 Print-Usage(){
-    printf "Usage: %s [-p <BackupPath>] [-e <RemoteShell>] [-f <ExcludeFiles>]\\n" "$0"
+    printf "Usage: %s [-l] [-s source] [-p <BackupPath>] [-e <RemoteShell>] [-f <ExcludeFiles>] [-r <RsyncArgs>]\\n" "$0" >&2
     exit 0
 }
-function trapped {
-    printf \\n
-    lvm_exit
-}
 function lvm_exit {
+    printf \\n
     if test "$lvm"; then
       if mountpoint -q "$source"; then
         umount "$source" || exit 1
@@ -53,6 +50,9 @@ while getopts ":p:e:f:ls:" arg; do
     l)
       lvm="lvm"
       ;;
+    r)
+      RsyncArgs=${OPTARG}
+      ;;
     \?)
       Print-Usage
       ;;
@@ -61,9 +61,12 @@ done
 shift $((OPTIND-1))
 if test "$lvm"; then
     lvm="$(df --output=source "$source" | tail -1)"    #e.g. /dev/mapper/ssd-root
+    if [ "/dev/mapper" != "$(dirname "$lvm")" ]; then
+        printf "Source isn't a filesystem on a logical volume" >&2
+    fi
     lvm_snap="$(basename "$lvm")"                    #-> ssd-root
     lvm_VG="${lvm%%-*}"                                #-> /dev/mapper/ssd
-    lvm_snap="${lvm#*-}-snap-$(date +%F-%0H-%0M)"    #-> root-snap-d2018-03-16-13-31
+    lvm_snap="${lvm#*-}-snap-$(date +%F-%0H-%0M)"    #-> root-snap-2018-03-16-13-31
     source="$(mktemp -d /media/temp.XXX)" && \
     test "$lvm_snap" && test "$lvm_VG" || lvm_exit 1
     trap lvm_exit INT
@@ -71,9 +74,9 @@ fi
 
 ###################### UI ######################
 if [ "$(df --output=source / | tail -1)" == "$(df --output=source "$BackupPath" | tail -1)" ]; then
-    printf "Warning: BackupPath (\"%s\") is on the same Filesystem as the Root Filesystem (\"/\")\\n" "$BackupPath"
+    printf "Warning: BackupPath (\"%s\") is on the same Filesystem as the Root Filesystem (\"/\")\\n" "$BackupPath" >&2
 fi
-printf    "${lvm:+lvm mapping: \t$lvm\\n}\
+printf "${lvm:+lvm mapping: \t$lvm\\n}\
 ${lvm_snap:+lv snap: \t$(basename "$lvm_VG")/$lvm_snap\\n}\
 source: \t%s
 BackupPath: \t%s
@@ -94,6 +97,6 @@ if test "$lvm"; then
     lvcreate -L 5G --snapshot -n "$lvm_snap" "$lvm" && \
     mount "$lvm_VG-${lvm_snap//-/--}" "$source" || lvm_exit 1
 fi
-rsync ${RemoteShell:+-e "$RemoteShell"} -i -ahxHAX --delete ${ExcludeFiles:+"--exclude-from=$ExcludeFiles"} "$source"/ "$BackupPath"/
+rsync ${RemoteShell:+-e "$RemoteShell"} -i --stats -ahHAX --delete ${ExcludeFiles:+"--exclude-from=$ExcludeFiles"} "$source"/ "$BackupPath"/
 lvm_exit
 
